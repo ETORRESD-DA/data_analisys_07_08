417df9bef550c4bbd7513ec481548252b561d1ff
ETORRESD <etorresdelucio@hotmail.com>
2020-09-05 12:27:55 -0500

    Actualización

---
+++ 002_web_scrapping.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+# Requests to web server to get HTML Plain
+BASE_URL = 'https://realpython.com/'
+
+response = requests.get(BASE_URL)
+response_content = response.content
+
+# BS Implementation
+soup = BeautifulSoup(response_content, 'html.parser')
+
+#print(soup)
+#print(soup.title)
+#print(soup.title.string)
+links = soup.find_all('a')
+print(links)
+print(len(links))
